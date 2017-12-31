c4cfed73d0a73a9e54d5438213368db396a59f98
vcabales <vcabales96@gmail.com>
2017-12-30 22:56:56 -0500

    toggling between different waves

--- src/App.js
+++ src/App.js
@@ -4,14 +4,72 @@ import './App.css';
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 //only create audio context once
 
+/* make canvas continuously draw, regardless of sound */
+class CanvasComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.draw = this.draw.bind(this);
+      this.handleClick = this.handleClick.bind(this);
+    }
+    componentDidMount() { //only called one time, after the HTML is
+      this.updateCanvas();
+    }
+    updateCanvas() {
+      this.canvasContext = this.refs.canvas.getContext('2d');
+      this.canvasContext.fillStyle = 'rgb(200,200,200)';
+      this.canvasContext.fillRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+
+      if (!this._drawVisual) { //initialize requestAnimationFrame
+        this._drawVisual = window.requestAnimationFrame(this.draw); //draws automatically, resets every 60 frames
+      }
+    }
+    handleClick(e) {
+      const play = e.target.value;
+      this.props.onClick(play); /* update prop for child component */
+    }
+    draw() {
+
+      /* //for debugging
+      this.canvasContext.font = "28px Georgia";
+      this.canvasContext.fillStyle = "blue";
+      this.canvasContext.fillText("Testing draw function", 10, 50); */
+
+      this.canvasContext.lineWidth = 2;
+      this.canvasContext.strokeStyle = 'rgb(0,0,0)';
+      this.canvasContext.beginPath();
+      var sliceWidth = this.refs.canvas.width * 1.0 / this.props.bufferLength;
+      var x=0;
+      for (var i = 0; i < this.props.bufferLength; i++) {
+        var v = this.props.dataArray[i] / 128.0;
+        var y = v * this.refs.canvas.height / 2;
+        if (i === 0) {
+          this.canvasContext.moveTo(x, y);
+        } else {
+          this.canvasContext.lineTo(x, y);
+        }
+        x += sliceWidth;
+      }
+      this.canvasContext.lineTo(this.refs.canvas.width, this.refs.canvas.height / 2);
+      this.canvasContext.stroke();
+      //set up next iteration of loop
+      this._drawVisual = window.requestAnimationFrame(this.draw); /* currently not changing, supposed to update every 60 frames*/
+
+    }
+    render() {
+        return (
+            <div>
+              <canvas ref="canvas" width={300} height={256}/>
+              <button onClick={this.handleClick}>Play</button>
+            </div>
+        );
+    }
+}
+
 class Sound extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {play: false};
     this.play = this.play.bind(this);
-  }
-  componentDidMount() {
-    this.init();
+    this.state = {play: true};
   }
   init() {
     this.context = audioContext;
@@ -33,9 +91,9 @@ class Sound extends React.Component {
     this.oscillator.frequency.value = 440; //set to A TODO: make frequency customizable
 
     /* volume control initialization */
-    this.oscillator.start(0.5);
+    this.oscillator.start(this.context.currentTime);
     this.gainNode = this.context.createGain(); //gain node controls volume
-    this.gainNode.gain.value = 0;
+    this.gainNode.gain.value = 1;
     this.oscillator.connect(this.gainNode); //connect oscillator to volume control
     this.gainNode.connect(this.context.destination);
 
@@ -51,83 +109,92 @@ class Sound extends React.Component {
   play() {
     const playSound = !this.state.play;
     this.setState({play: playSound});
+    if (this.state.play === true) { //state is just set on first click
+      this.init();
+    }
+    else {
+      this.gainNode.gain.value = 0;
+    }
   }
-
   render() {
     return (
-      <div className="soundInit">
         <CanvasComponent bufferLength={this.bufferLength} dataArray={this.dataArray} play={this.state.play} onClick={this.play}/>
+    );
+  }
+}
+
+class Wave extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wave: 'sine',
+      frequency: 440
+  }; //default to sine wave
+    this.changeWave = this.changeWave.bind(this);
+    this.changeFrequency = this.changeFrequency.bind(this);
+  }
+  changeWave(newWave) {
+    this.setState({wave: newWave});
+  }
+  changeFrequency(newFrequency) {
+    this.setState({frequency: newFrequency});
+  }
+  render() {
+
+    return(
+      <div>
+        <Sound wave={this.state.wave}/>
+        <WaveContainer onChange={this.changeWave}/>
+        <SliderContainer onChange={this.changeFrequency}/>
       </div>
     );
   }
 }
 
-/* make canvas continuously draw, regardless of sound */
-class CanvasComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.draw = this.draw.bind(this);
-      this.handleClick = this.handleClick.bind(this);
-    }
-    componentDidMount() { //only called one time, gives access to refs of component's children
-      this.updateCanvas();
-    }
-    updateCanvas() {
-      this.canvasContext = this.refs.canvas.getContext('2d');
-      this.canvasContext.fillStyle = 'rgb(200,200,200)';
-      this.canvasContext.fillRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
-      if (!this.drawVisual) {
-        this.drawVisual = window.requestAnimationFrame(this.draw);
-      }
-    }
-    handleClick(e) {
-      const play = e.target.value;
-      this.props.onClick(play); /* update prop for child component */
+class WaveContainer extends React.Component { //handles change
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    const wave = e.target.value;
+    this.props.onChange(wave);
+  }
+  render() {
+    return(
+      <div>
+        <select onChange={this.handleChange}>
+          <option value="sine">sine</option>
+          <option value="sawtooth">sawtooth</option>
+          <option value="square">square</option>
+          <option value="triangle">triangle</option>
+        </select>
+      </div>
+    );
+  }
+}
 
-      if (this.props.play) {
-        this.draw(); //only working on second click and not toggling?
-      }
-    }
-    draw() {
-      /*
-      this.canvasContext.font = "28px Georgia";
-      this.canvasContext.fillStyle = "blue";
-      this.canvasContext.fillText("Testing draw function", 10, 50);
-      */
-      this.canvasContext.lineWidth = 2;
-      this.canvasContext.strokeStyle = 'rgb(0,0,0)';
-      this.canvasContext.beginPath();
-      var sliceWidth = this.refs.canvas.width * 1.0 / this.props.bufferLength;
-      var x=0;
-      for (var i = 0; i < this.props.bufferLength; i++) {
-        var v = this.props.dataArray[i] / 128.0;
-        var y = v * this.refs.canvas.height / 2;
-        if (i === 0) {
-          this.canvasContext.moveTo(x, y);
-        } else {
-          this.canvasContext.lineTo(x, y);
-        }
-        x += sliceWidth;
-      }
-      this.canvasContext.lineTo(this.refs.canvas.width, this.refs.canvas.height / 2);
-      this.canvasContext.stroke();
-    }
-    render() {
-        return (
-            <div>
-              <canvas ref="canvas" width={300} height={256}/>
-              <button onClick={this.handleClick}>Play</button>
-            </div>
-        );
-    }
+class SliderContainer extends React.Component { //control frequency range
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    const frequency = e.target.value;
+    this.props.onChange(frequency);
+  }
+  render() {
+    return(
+      <input onChange={this.handleChange} type="range" min="16.35" max="7902.13" value="440" className="slider" id="myRange" />
+    );
+  }
 }
 
 class App extends Component {
   render() {
-
     return (
-      <div className="App">
-        <Sound wave="sine" />
+      <div>
+        <Wave />
       </div>
     );
   }
